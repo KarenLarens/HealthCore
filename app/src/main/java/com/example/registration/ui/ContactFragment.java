01a82182e97e1d6f32718d90dc8f502a34c62ed8
XIMENAVELASCO0403 <93949904+XIMENAVELASCO0403@users.noreply.github.com>
2021-12-12 01:27:12 -0600

    RedesSocialesContacto

--- app/src/main/java/com/example/registration/ui/ContactFragment.java
+++ app/src/main/java/com/example/registration/ui/ContactFragment.java
@@ -1,9 +1,12 @@
 package com.example.registration.ui;
-
+//CONTACTO
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +15,30 @@ import androidx.fragment.app.Fragment;
 import com.example.registration.R;
 
 public class ContactFragment extends Fragment {
+    private ImageButton btnLogoFace, btnLogoInsta;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+        ImageButton entry = view.findViewById(R.id.btnLogoFace);
+        entry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.facebook.com/HealthCore-101091559105887/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        ImageButton entry2 = view.findViewById(R.id.btnLogoInsta);
+        entry2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.instagram.com/invites/contact/?i=5mh754mnxvyc&utm_content=neaqs6l");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
