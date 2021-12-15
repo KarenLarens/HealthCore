6c8585f6a85b8f2ce614be4b883f0d349fe8b765
RangelDJesus <18151688@aguascalientes.tecnm.mx>
2021-12-14 20:45:50 -0600

    Firebase profilev2

--- app/src/main/java/com/example/registration/ui/ProfileFragment2.java
+++ app/src/main/java/com/example/registration/ui/ProfileFragment2.java
@@ -4,20 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.registration.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class ProfileFragment2 extends Fragment {
 
+    DatabaseReference reference;
+    Switch Genre;
+    EditText NombreET,ApePatET,ApeMatET,AgeET, UserET,EmailET,PassET;
+    Button submit;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile2, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile2, container, false);
+
+        Genre = (Switch) view.findViewById(R.id.GenreSwitch);
+
+        NombreET= (EditText) view.findViewById(R.id.NombreET);
+        ApePatET= (EditText) view.findViewById(R.id.ApePatET);
+        ApeMatET= (EditText) view.findViewById(R.id.ApeMatET);
+        AgeET= (EditText) view.findViewById(R.id.AgeET);
+        UserET= (EditText) view.findViewById(R.id.UserET);
+        EmailET= (EditText) view.findViewById(R.id.EmailT);
+        PassET= (EditText) view.findViewById(R.id.PassET);
+
+        submit = (Button) view.findViewById(R.id.BtnActualizar);
+
+
+        return view;
+    }
+
+    public void MostrarDatos(){
+        reference= FirebaseDatabase.getInstance().getReference().child("Perfil");
+
     }
 
 }
